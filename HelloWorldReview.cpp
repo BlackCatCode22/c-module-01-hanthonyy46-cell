4c85230ce3f358f0c2592c1bbb56bb240038c55a
hanthonyy46-cell <hanthonyy46@gmail.com>
2025-08-13 11:52:25 -0700

    Add files via upload

---
+++ HelloWorldReview.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    cout << "Hello World!";
+    int a = 10;
+    int b = 20;
+    cout <<"the value of a is " << a << endl;
+    cout <<"the value of b is " << b << endl;
+
+
+
+    return 0;
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
