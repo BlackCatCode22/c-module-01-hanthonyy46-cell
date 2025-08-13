089f45339870241f003179fc762fcfafed4bfbc6
hanthonyy46-cell <hanthonyy46@gmail.com>
2025-08-13 12:22:24 -0700

    Add files via upload

---
+++ addtwonumbers.cpp
@@ -0,0 +1,18 @@
+//
+// Created by BE129 on 8/13/2025.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int sumofnums = 0;
+    int num1 = 0;
+    int num2 = 0;
+    cout << "enter a value for num1:";
+    cin >> num1;
+    cout << "enter a whole number for num2:";
+    cin >> num2;
+    sumofnums = num1 + num2;
+    cout << "the sum of num1 + num2 = " << sumofnums << endl;
+    return 0;
+}
