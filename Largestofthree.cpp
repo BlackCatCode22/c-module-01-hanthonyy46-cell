4fdb3c5233e2769d7d2ce70d2385bccbd8706a21
hanthonyy46-cell <hanthonyy46@gmail.com>
2025-08-18 11:59:44 -0700

    Add files via upload

---
+++ Largestofthree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout <<"Welcome to my ints program. /n";
+    int num1;
+    int num2;
+    int num3;
+    int sumoftheints;
+    num1 = 0;
+    num2 = 0;
+    num3 = 0;
+    sumoftheints = 0;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "/n your entered" << num1 << "for num1";
+    cout << "enter the second number: ";
+    cin >> num2;
+    cout << "/n your entered" << num2 << "for num2";
+    cout << "enter the third number: ";
+    cin >> num3;
+    cout << "/n your entered" << num3 << "for num3";
+
+
+}
+
+int anotherfuntiion() {
+
+    int num1;
+
+    return 0;
+}
